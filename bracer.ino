e5b3ff83df745b1d2d2f7e6d43f3a2d52ed5463b
Benjamin Farahmand <ben@tuvalabs.com>
2022-08-13 14:15:35 -0700

    Add files via upload

---
+++ bracer.ino
@@ -0,0 +1,54 @@
+#include <TFT_eSPI.h> // Hardware-specific library
+#include <SPI.h>
+#include "DigitalWatch.h"
+#include "Adafruit_TSC2007.h"
+
+#define TFT_GREY 0x5AEB
+
+TFT_eSPI tft = TFT_eSPI();       // Invoke custom library
+Adafruit_TSC2007 touch;
+
+DigitalWatch dw = DigitalWatch();
+
+unsigned int colour = 0;
+
+void setup(void) {
+//  Serial.begin(115200);
+  
+  tft.init();
+  tft.setRotation(1);
+  tft.fillScreen(TFT_BLACK);
+
+  tft.setTextSize(1);
+  tft.setTextColor(TFT_YELLOW, TFT_BLACK);
+
+  if (!touch.begin()) {
+    while (1) { delay(10); }
+  }
+
+  dw.init();
+
+}
+
+void loop() {
+  dw.drawTime(tft);
+
+  //TOUCH SENSOR
+  uint16_t x, y, z1, z2;
+  double maxX=0.078125, maxY=0.05859375;
+  if (touch.read_touch(&x, &y, &z1, &z2) && (z1 > 100)) {
+    x = 320-((double)x)*maxX;
+    y = 240-((double)y)*maxY;
+    
+//    Serial.print("Touch point: (");
+//    Serial.print(x); Serial.print(", ");
+//    Serial.print(y); Serial.print(", ");
+//    Serial.print(z1); Serial.print(" / ");
+//    Serial.print(z2); Serial.println(")");
+    
+    tft.drawSpot(x,y,25,TFT_WHITE);
+
+    
+  }
+  
+}
