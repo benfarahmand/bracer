a91b22d4448016b3cbc7142155f709f0571662ee
benfarahmand <ben@tuvalabs.com>
2022-08-24 17:32:14 -0400

    udpate

--- bracer.ino
+++ bracer.ino
@@ -14,8 +14,6 @@ RTC_DATA_ATTR int bootCount = 0;
 
 ESP32Time rtc(4 * 3600);
 
-//touch screen
-Adafruit_TSC2007 touch;
 
 // Connect to the GPS on the hardware I2C port
 Adafruit_GPS GPS(&Wire);
@@ -165,43 +163,6 @@ Adafruit_ILI9341 GPSParse(Adafruit_ILI9341 tft) {
     //    }
   }
   return tft;
-}
-
-void initSCD41() {
-  uint16_t error;
-  char errorMessage[256];
-
-  scd4x.begin(Wire);
-
-  // stop potentially previously started measurement
-  error = scd4x.stopPeriodicMeasurement();
-  if (error) {
-    Serial.print("Error trying to execute stopPeriodicMeasurement(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  }
-
-  uint16_t serial0;
-  uint16_t serial1;
-  uint16_t serial2;
-  error = scd4x.getSerialNumber(serial0, serial1, serial2);
-  if (error) {
-    Serial.print("Error trying to execute getSerialNumber(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  } else {
-    printSerialNumber(serial0, serial1, serial2);
-  }
-
-  // Start Measurement
-  error = scd4x.startPeriodicMeasurement();
-  if (error) {
-    Serial.print("Error trying to execute startPeriodicMeasurement(): ");
-    errorToString(error, errorMessage, 256);
-    Serial.println(errorMessage);
-  }
-
-  Serial.println("Waiting for first measurement... (5 sec)");
 }
 
 #define BUTTON_PIN_BITMASK 0x8000000000 // 2^39 in hex, GPI 39
